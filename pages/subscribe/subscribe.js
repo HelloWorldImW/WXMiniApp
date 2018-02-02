1fe0c3fdca962ddb555e631c994eba640599a09e
Darren <sdauwangzh@163.com>
2018-02-02 16:51:00 +0800

    [modify]修改预约界面

--- pages/subscribe/subscribe.js
+++ pages/subscribe/subscribe.js
@@ -5,7 +5,34 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    screenWidth: wx.getSystemInfoSync().windowWidth,
+    screenHeight:wx.getSystemInfoSync().windowHeight,
+    name: {
+      icon:"/resource/location.png",
+      title:"姓名",
+    },
+    telphone: {
+      icon: "/resource/location.png",
+      title: "联系电话",
+    },
+    chooseClass: {
+      icon: "/resource/location.png",
+      title: "所选课程",
+      pullDownIcon:"/resource/location.png",
+    },
+    remark: {
+      icon: "/resource/location.png",
+      title: "备注",
+      color: "gray",
+    },
+    array: ['美国美国美国', '中国中国中国', '巴西巴西巴西', '日本日本日本'],
+    index:0,
+  },
+
+  bindPickerChange: function (e) {
+    this.setData({
+      index: e.detail.value
+    })
   },
 
   /**
@@ -14,53 +41,4 @@ Page({
   onLoad: function (options) {
   
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
